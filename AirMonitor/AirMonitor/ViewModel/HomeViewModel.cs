9c6978ad8e4799a9a92d55f59e00c9649267ea15
arekbystrowicz <arek.bystrowicz@gamil.com>
2020-06-07 15:36:42 +0200

    implement HomeViewModel

---
+++ AirMonitor/AirMonitor/ViewModel/HomeViewModel.cs
@@ -0,0 +1,22 @@
+using System.Windows.Input;
+using AirMonitor.Views;
+using Xamarin.Forms;
+
+namespace AirMonitor.ViewModel
+{
+    public class HomeViewModel
+    {
+        private readonly INavigation _navigation;
+
+        public HomeViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+        }
+        
+        private ICommand _goToDetailsCommand;
+        public ICommand GoToDetailsCommand =>
+            _goToDetailsCommand ?? (_goToDetailsCommand = new Command(OnGoToDetails));
+        
+        private void OnGoToDetails() => _navigation.PushAsync(new DetailsPage());
+    }
+}
