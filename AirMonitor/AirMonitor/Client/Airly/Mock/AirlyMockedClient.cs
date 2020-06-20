a34893d3fe2e07ec6180d2d0c45aa0801f9e88df
arekbystrowicz <arek.bystrowicz@gamil.com>
2020-06-20 15:18:43 +0200

    delete debug lines

--- AirMonitor/AirMonitor/Client/Airly/Mock/AirlyMockedClient.cs
+++ AirMonitor/AirMonitor/Client/Airly/Mock/AirlyMockedClient.cs
@@ -12,19 +12,15 @@ namespace AirMonitor.Client.Airly.Mock
     {
         public async Task<IEnumerable<ApiInstallation>> GetInstallations(LocationMapping location, double maxDistanceInKm = 3, int maxResults = -1)
         {
-            Console.WriteLine("XXX_I");
             var json = AirlyMockedInstallationsJson.Get();
             var installations = JsonConvert.DeserializeObject<IEnumerable<ApiInstallation>>(json);
-            Console.WriteLine(installations);
             return installations;
         }
 
         public async Task<ApiMeasurement> GetMeasurementForInstallation(int installationId)
         {
-            Console.WriteLine("XXX_M");
             var json = AirlyApiMockedMeasurementJson.Get(installationId);
             var measurement = JsonConvert.DeserializeObject<ApiMeasurement>(json);
-            Console.WriteLine(measurement);
             return measurement;
         }
     }
